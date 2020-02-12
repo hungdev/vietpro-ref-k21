f3d7dfc098d2838d0696f4955cd940f4254b1234
Cee <cee@Cees-MacBook-Pro.local>
2020-02-12 20:06:16 +0700

    add modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import ModalRef from './ModalRef'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center', flexDirection: 'column',
+      height: '100%'
+    }}>
+      <Button type="primary">Primary</Button>
+      <ModalRef
+      />
     </div>
   );
 }
